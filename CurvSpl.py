172612133354205c739245b444f63c7c330e30b5
Taku Nakai <tnakaicode@gmail.com>
2019-10-26 14:38:02 +0900

    spline curve

--- CurvSpl.py
+++ CurvSpl.py
@@ -5,8 +5,8 @@ import sys
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
-from OCC.Core.gp import gp_Lin, gp_Sphere
-from OCC.Core.gp import gp_Mat, gp_GTrsf, gp_XYZ
+from OCC.Core.gp import gp_Lin, gp_Pln, gp_Sphere
+from OCC.Core.gp import gp_Mat, gp_XYZ
 from OCC.Core.gp import gp_Trsf, gp_GTrsf
 from OCC.Core.TColgp import TColgp_Array1OfPnt, TColgp_Array2OfPnt
 from OCC.Core.TColgp import TColgp_HArray1OfPnt, TColgp_HArray2OfPnt
@@ -61,6 +61,21 @@ def spl_face(px, py, pz):
     return BRepBuilderAPI_MakeFace(api.Surface(), 1e-6).Face()
 
 
+def spl_curv(px, py, pz):
+    num = px.size
+    pts = []
+    p_array = TColgp_Array1OfPnt(1, num)
+    for idx, t in enumerate(px):
+        x = px[idx]
+        y = py[idx]
+        z = pz[idx]
+        pnt = gp_Pnt(x, y, z)
+        pts.append(pnt)
+        p_array.SetValue(idx + 1, pnt)
+    api = GeomAPI_PointsToBSpline(p_array)
+    return p_array, api.Curve()
+
+
 if __name__ == '__main__':
     obj = plotocc()
     obj.show_axs_pln(scale=20)
@@ -70,27 +85,23 @@ if __name__ == '__main__':
     mesh = np.meshgrid(px, py)
     surf = mesh[0]**2 / 100 + mesh[1]**2 / 1000
     surf[7, 5] = 50
-    
+
     p0 = gp_Pnt(mesh[0][0, 0], mesh[1][0, 0], surf[0, 0])
     p1 = gp_Pnt(mesh[0][0, -1], mesh[1][0, -1], surf[0, -1])
     p2 = gp_Pnt(mesh[0][-1, 0], mesh[1][-1, 0], surf[-1, 0])
     p3 = gp_Pnt(mesh[0][-1, 1], mesh[1][-1, -1], surf[-1, -1])
 
     pt = np.linspace(0, 2 * np.pi, 100)
-    pts = []
-    p_array = TColgp_Array1OfPnt(1, 100)
-    for idx, t in enumerate(pt):
-        x = 30.0 * np.cos(t)
-        y = 30.0 * np.sin(t)
-        z = 15 * np.cos(3 * t)
-        pnt = gp_Pnt(x, y, z)
-        pts.append(pnt)
-        p_array.SetValue(idx + 1, pnt)
-    api = GeomAPI_PointsToBSpline(p_array)
-    #curve = geomapi.To3d(api.Curve())
+    px = 30.0 * np.cos(pt)
+    py = 30.0 * np.sin(pt)
+    pz = 15 * np.cos(5 * pt)
+    curv_pts, curv_shp = spl_curv(px, py, pz)
 
-    for idx, t in enumerate(pts):
-        obj.display.DisplayShape(t)
-    obj.display.DisplayShape(api.Curve())
+    #api = geomapi.To3d(curv_shp)
+    #obj.display.DisplayShape(api)
+
+    for idx in range(curv_pts.Lower(), curv_pts.Upper() + 1):
+        obj.display.DisplayShape(curv_pts.Value(idx))
+    obj.display.DisplayShape(curv_shp)
 
     obj.show()
