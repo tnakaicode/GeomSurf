8cc77faf93bce1e2c6283a64d5e90a1575a7bd7f
Taku Nakai <tnakaicode@gmail.com>
2019-10-27 01:31:14 +0900

    ellipse location

--- Ellipsoid.py
+++ Ellipsoid.py
@@ -8,6 +8,8 @@ from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
 from OCC.Core.gp import gp_Lin, gp_Sphere
 from OCC.Core.gp import gp_Mat, gp_GTrsf, gp_XYZ
 from OCC.Core.gp import gp_Trsf, gp_GTrsf
+from OCC.Core.TopoDS import TopoDS_Shape
+from OCC.Core.TopLoc import TopLoc_Location
 from OCC.Core.TColgp import TColgp_Array1OfPnt, TColgp_Array2OfPnt
 from OCC.Core.TColgp import TColgp_HArray1OfPnt, TColgp_HArray2OfPnt
 from OCC.Core.BRepFill import BRepFill_Filling
@@ -26,6 +28,18 @@ from OCCUtils.Construct import make_edge
 from base import plotocc
 
 
+def set_trf(ax1=gp_Ax3(), ax2=gp_Ax3()):
+    trf = gp_Trsf()
+    trf.SetTransformation(ax2, ax1)
+    return trf
+
+
+def set_loc(ax1=gp_Ax3(), ax2=gp_Ax3()):
+    trf = set_trf(ax1, ax2)
+    loc = TopLoc_Location(trf)
+    return loc
+
+
 def trsf_scale(axs=gp_Ax3(), scale=1):
     trf = gp_Trsf()
     trf.SetDisplacement(gp_Ax3(), axs)
@@ -33,14 +47,16 @@ def trsf_scale(axs=gp_Ax3(), scale=1):
 
 
 def gen_ellipsoid(axs=gp_Ax3(), rxyz=[10, 20, 30]):
+    sphere = BRepPrimAPI_MakeSphere(gp_Ax2(), 1).Solid()
+    loc = set_loc(gp_Ax3(), axs)
     mat = gp_Mat(
         rxyz[0], 0, 0,
         0, rxyz[1], 0,
         0, 0, rxyz[2]
     )
     gtrf = gp_GTrsf(mat, gp_XYZ(0, 0, 0))
-    sphere = BRepPrimAPI_MakeSphere(axs.Ax2(), 1).Solid()
     ellips = BRepBuilderAPI_GTransform(sphere, gtrf).Shape()
+    ellips.Location(loc)
     return ellips
 
 
@@ -50,6 +66,11 @@ if __name__ == '__main__':
     # obj.show_ball()
 
     axs = gp_Ax3(gp_Pnt(0, 0, 0), gp_Dir(1, 1, 1))
+    elp = gen_ellipsoid(axs, [10, 20, 30])
+    obj.display.DisplayShape(elp, transparency=0.7, color="BLUE")
+    obj.show_axs_pln(axs, scale=20)
+
+    axs = gp_Ax3(gp_Pnt(30, 0, 0), gp_Dir(1, 1, 0))
     elp = gen_ellipsoid(axs, [10, 20, 30])
     obj.display.DisplayShape(elp, transparency=0.7, color="BLUE")
     obj.show_axs_pln(axs, scale=20)
