cacf02c5f5bebb2d811b86989c065c6c99f8ef27
Taku Nakai <tnakaicode@gmail.com>
2019-10-27 01:17:00 +0900

    ellipoid

---
+++ Ellipsoid.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+from OCC.Display.SimpleGui import init_display
+from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
+from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.Core.gp import gp_Lin, gp_Sphere
+from OCC.Core.gp import gp_Mat, gp_GTrsf, gp_XYZ
+from OCC.Core.gp import gp_Trsf, gp_GTrsf
+from OCC.Core.TColgp import TColgp_Array1OfPnt, TColgp_Array2OfPnt
+from OCC.Core.TColgp import TColgp_HArray1OfPnt, TColgp_HArray2OfPnt
+from OCC.Core.BRepFill import BRepFill_Filling
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeSphere
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeFace
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_GTransform
+from OCC.Core.GeomAPI import geomapi
+from OCC.Core.GeomAPI import GeomAPI_PointsToBSplineSurface
+from OCC.Core.GeomAPI import GeomAPI_PointsToBSpline
+from OCC.Core.GeomAPI import GeomAPI_Interpolate
+from OCC.Core.GeomAbs import GeomAbs_C0, GeomAbs_C1, GeomAbs_C2
+from OCC.Core.GeomAbs import GeomAbs_G1, GeomAbs_G2
+from OCCUtils.Construct import make_n_sided, make_n_sections
+from OCCUtils.Construct import make_edge
+
+from base import plotocc
+
+
+def trsf_scale(axs=gp_Ax3(), scale=1):
+    trf = gp_Trsf()
+    trf.SetDisplacement(gp_Ax3(), axs)
+    return trf
+
+
+def gen_ellipsoid(axs=gp_Ax3(), rxyz=[10, 20, 30]):
+    mat = gp_Mat(
+        rxyz[0], 0, 0,
+        0, rxyz[1], 0,
+        0, 0, rxyz[2]
+    )
+    gtrf = gp_GTrsf(mat, gp_XYZ(0, 0, 0))
+    sphere = BRepPrimAPI_MakeSphere(axs.Ax2(), 1).Solid()
+    ellips = BRepBuilderAPI_GTransform(sphere, gtrf).Shape()
+    return ellips
+
+
+if __name__ == '__main__':
+    obj = plotocc()
+    # obj.show_axs_pln(scale=20)
+    # obj.show_ball()
+
+    axs = gp_Ax3(gp_Pnt(0, 0, 0), gp_Dir(1, 1, 1))
+    elp = gen_ellipsoid(axs, [10, 20, 30])
+    obj.display.DisplayShape(elp, transparency=0.7, color="BLUE")
+    obj.show_axs_pln(axs, scale=20)
+
+    obj.show()
