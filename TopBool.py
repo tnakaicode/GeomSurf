1a899ad002a200188be6703f8e98b79646d2e033
Taku Nakai <t.nakai.office@gmail.com>
2019-11-18 00:55:32 +0900

    import

--- TopBool.py
+++ TopBool.py
@@ -9,20 +9,28 @@ from OCC.gp import gp_Lin, gp_Pln, gp_Sphere
 from OCC.gp import gp_Mat, gp_XYZ
 from OCC.gp import gp_Trsf, gp_GTrsf
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeWedge
+from OCC.BRepPrimAPI import BRepPrimAPI_MakePrism
+from OCC.BRepAlgoAPI import BRepAlgoAPI_Common
+from OCC.BRepAlgoAPI import BRepAlgoAPI_Fuse
+from OCC.BRepAlgoAPI import BRepAlgoAPI_Section
+from OCC.BRepSweep import BRepSweep_Prism
+from OCC.BRepOffsetAPI import BRepOffsetAPI_MakePipe
+from OCC.BRepOffsetAPI import BRepOffsetAPI_ThruSections
 from OCCUtils.Construct import make_n_sided, make_n_sections
 from OCCUtils.Construct import make_edge
 
-from base import plotocc
-
-
+from base import plotocc, gen_ellipsoid
 
 
 if __name__ == '__main__':
     obj = plotocc()
     obj.show_axs_pln(scale=20)
 
+    elli1 = gen_ellipsoid(axs=gp_Ax3(gp_Pnt(-90, 0, 0),
+                                     gp_Dir(0, 0, 1)), rxyz=[100, 100, 100])
     Wedge = BRepPrimAPI_MakeWedge(60., 100., 80., 20.).Shape()
+
+    obj.display.DisplayShape(elli1)
     obj.display.DisplayShape(Wedge)
 
-    
     obj.show()
