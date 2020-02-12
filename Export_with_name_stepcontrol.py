d5d1ab1af72a104bcccd237a50043383b714c37a
Taku Nakai <tnakaicode@gmail.com>
2020-02-12 15:59:29 +0900

    export with name

--- Export_with_name_stepcontrol.py
+++ Export_with_name_stepcontrol.py
@@ -5,31 +5,17 @@ import os
 
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.gp import gp_Pnt
-from OCC.Core.XSControl import XSControl_Writer, XSControl_WorkSession
-from OCC.Core.XCAFApp import XCAFApp_Application
-from OCC.Core.XCAFDoc import (XCAFDoc_DocumentTool_ShapeTool,
-                              XCAFDoc_DocumentTool_ColorTool,
-                              XCAFDoc_DocumentTool_LayerTool,
-                              XCAFDoc_DocumentTool_MaterialTool)
-from OCC.Core.STEPCAFControl import STEPCAFControl_Writer
+from OCC.Core.STEPConstruct import stepconstruct_FindEntity
 from OCC.Core.STEPControl import STEPControl_Writer
-from OCC.Core.STEPControl import (STEPControl_AsIs,
-                                  STEPControl_ManifoldSolidBrep,
-                                  STEPControl_FacetedBrep,
-                                  STEPControl_ShellBasedSurfaceModel,
-                                  STEPControl_GeometricCurveSet)
+from OCC.Core.STEPControl import STEPControl_AsIs
 from OCC.Core.Interface import Interface_Static_SetCVal
-from OCC.Core.IFSelect import IFSelect_RetDone
-from OCC.Core.TDF import TDF_LabelSequence, TDF_Label, TDF_Tool, TDF_Data
+from OCC.Core.IFSelect import IFSelect_RetDone, IFSelect_RetError
 from OCC.Core.TDataStd import TDataStd_Name, TDataStd_Name_GetID
-from OCC.Core.TDataStd import TDataStd_Name
+from OCC.Core.TCollection import TCollection_HAsciiString
 from OCC.Core.TCollection import TCollection_AsciiString
 from OCC.Core.TCollection import TCollection_ExtendedString
-from OCC.Core.TDocStd import TDocStd_Document
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
-from OCC.Core.STEPControl import STEPControl_Writer
 from OCC.Extend.DataExchange import write_step_file, read_step_file
-from OCC.Extend.DataExchange import read_step_file_with_names_colors
 from OCCUtils.Construct import make_plane, make_vertex, make_circle
 
 
@@ -40,15 +26,11 @@ class ExportCAFMethod (object):
 
     def __init__(self, name="name", tol=1.0E-10):
         self.name = name
-        self.step = STEPCAFControl_Writer()
-        self.step.SetNameMode(True)
-        self.doc = TDocStd_Document(
-            TCollection_ExtendedString("pythonocc-doc"))
-        self.x_app = XCAFApp_Application.GetApplication()
-        self.x_app.NewDocument(
-            TCollection_ExtendedString("MDTV-CAF"), self.doc)
-        self.shape_tool = XCAFDoc_DocumentTool_ShapeTool(self.doc.Main())
+        self.writer = STEPControl_Writer()
+        self.fp = self.writer.WS().TransferWriter().FinderProcess()
         Interface_Static_SetCVal("write.step.schema", "AP214")
+        Interface_Static_SetCVal('write.step.unit', 'mm')
+        Interface_Static_SetCVal('write.step.assembly', str(1))
 
     def Add(self, shape, name="name"):
         """
@@ -58,8 +40,17 @@ class ExportCAFMethod (object):
         STEPControl_ShellBasedSurfaceModel translates an Open CASCADE shape into a STEP shell_based_surface_model entity.
         STEPControl_GeometricCurveSet      translates an Open CASCADE shape into a STEP geometric_curve_set entity.
         """
-        label = self.shape_tool.AddShape(shape)
-        self.step.Transfer(self.doc, STEPControl_AsIs)
+        Interface_Static_SetCVal('write.step.product.name', name)
+        status = self.writer.Transfer(shape, STEPControl_AsIs)
+        if int(status) > int(IFSelect_RetError):
+            raise Exception('Some Error occurred')
+
+        # This portion is not working as I hoped
+        item = stepconstruct_FindEntity(self.fp, shape)
+        if not item:
+            raise Exception('Item not found')
+
+        item.SetName(TCollection_HAsciiString(name))
 
     def Write(self, filename=None):
         if not filename:
@@ -67,7 +58,7 @@ class ExportCAFMethod (object):
         path, ext = os.path.splitext(filename)
         if not ext:
             ext = ".stp"
-        status = self.step.Write(path + ext)
+        status = self.writer.Write(path + ext)
         assert(status == IFSelect_RetDone)
 
 
@@ -82,11 +73,4 @@ if __name__ == "__main__":
     root.Write()
 
     display.FitAll()
-
-    box = BRepPrimAPI_MakeBox(10, 10, 10).Solid()
-    writer = STEPControl_Writer()
-    fp = writer.WS().TransferWriter().FinderProcess()
-
-    print(fp)
-
-    # start_display()
+    start_display()
