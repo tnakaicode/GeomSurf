5a5cf34d827da809839af9a2aa4aeeb7bda14bd8
Taku CAE2018 <t.nakai.office@gmail.com>
2020-05-01 16:26:56 +0900

    test run

--- box_file.py
+++ box_file.py
@@ -3,8 +3,7 @@ import numpy as np
 from OCC.Core.gp import gp_Pnt
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
 from OCC.Extend.DataExchange import write_iges_file, write_step_file, write_stl_file
-from OCC.Extend.ShapeFactory import make_box
-
+from OCCUtils.Construct import make_box
 
 shp = make_box(gp_Pnt(-50, -50, -50), 100, 100, 100)
 write_iges_file(shp, "box.iges")
