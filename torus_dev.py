d87bc6718f023c032240adb4f3ab453de27f30a0
Taku Nakai <t.nakai.office@gmail.com>
2020-05-22 18:00:44 +0900

    torusw gtrsf

--- torus_dev.py
+++ torus_dev.py
@@ -13,8 +13,16 @@ logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.Core.gp import gp_Mat, gp_XYZ
 from OCC.Core.gp import gp_Lin, gp_Sphere
+from OCC.Core.gp import gp_GTrsf, gp_Trsf
+from OCC.Core.BRep import BRep_Tool
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeShape, BRepBuilderAPI_MakeFace
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_GTransform
 from OCC.Core.Geom import Geom_ToroidalSurface
+from OCC.Core.GeomLib import GeomLib_Tool
+from OCC.Core.ShapeAnalysis import ShapeAnalysis_Surface
+from OCC.Core.GeomLProp import GeomLProp_SLProps, GeomLProp_SurfaceTool
 from OCCUtils.Construct import make_n_sided, make_n_sections
 from OCCUtils.Construct import make_edge
 
@@ -23,13 +31,29 @@ class Torus (plotocc):
 
     def __init__(self):
         super().__init__()
-        self.t = Geom_ToroidalSurface(gp_Ax3(), 500, 100)
+        self.axs = gp_Ax3()
+        self.radi = [500, 200]
+        self.rxyz = [100, 200, 200]
+        mat = gp_Mat(
+            self.rxyz[0], 0, 0,
+            0, self.rxyz[1], 0,
+            0, 0, self.rxyz[2]
+        )
+        gtrf = gp_GTrsf(mat, gp_XYZ(0, 0, 0))
+        self.t = Geom_ToroidalSurface(self.axs, *self.radi)
+        self.face = BRepBuilderAPI_MakeFace(self.t, 1e-6).Face()
+        self.face = ellips = BRepBuilderAPI_GTransform(self.face, gtrf).Shape()
+        self.surf = BRep_Tool.Surface(self.face)
+        self.prop = GeomLProp_SLProps(self.surf, 0.0, 0.0, 1, 1.0)
+        self.export_stp(self.face)
         print(self.t.UPeriod())
 
     def get_prof(self, uv=[0, 0]):
         u, v = uv
+        u1, v1 = 2 * np.pi * u, 2 * np.pi * v
         p, vu, vv = gp_Pnt(), gp_Vec(), gp_Vec()
-        self.t.D1(2 * np.pi * u, 2 * np.pi * v, p, vu, vv)
+        self.t.D1(u1, v1, p, vu, vv)
+        self.prop.SetParameters(u1, v1)
         vx = vu.Normalized()
         vy = vv.Normalized()
         vz = vx.Crossed(vy)
@@ -38,12 +62,16 @@ class Torus (plotocc):
         print(vx)
         print(vy)
         print(vz)
+        # print(self.prop.GaussianCurvature())
+        # print(self.prop.MaxCurvature())
+        # print(self.prop.MinCurvature())
+        # print(self.prop.MeanCurvature())
         self.display.DisplayShape(p)
         self.display.DisplayVector(vz.Scaled(20), p)
         return p, vu, vv, vz
 
     def ShowTorus(self):
-        self.display.DisplayShape(self.t, transparency=0.7)
+        self.display.DisplayShape(self.face, transparency=0.7)
         self.show()
 
 
