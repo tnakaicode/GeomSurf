341361af51854ebc42f48ffc841dbaa64c02d1d6
tnakaicode <tnakaicode@gmail.com>
2022-12-28 15:03:08 +0900

    torus SPlop

---
+++ src/base_widget.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import os
+import time
+import shutil
+import subprocess
+import csv
+import argparse
+from datetime import date, datetime
+from linecache import getline, clearcache, updatecache
+
+basename = os.path.dirname(__file__) + "/"
+
+from PyQt5 import Qt, QtCore, QtGui, QtWidgets, sip
+from PyQt5.QtWidgets import qApp, QWidget, QApplication
+from PyQt5.QtWidgets import QMenu, QAction, QFileDialog
+from PyQt5.QtWidgets import QFormLayout, QHBoxLayout, QVBoxLayout, QGridLayout, QBoxLayout
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QTableView
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import QComboBox, QLabel, QLineEdit, QGroupBox, QPushButton
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+sys.path.append(os.path.join("./"))
+from base_qtApp import MainWindow
+from base import plot2d, create_tempnum
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+
+class NewWidget(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.left = 200
+        self.top = 200
+        self.width = 125
+        self.height = 125
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.setting = QtCore.QSettings(basename + "temp_setting/Widget.ini",
+                                        QtCore.QSettings.IniFormat)
+        self.setting.setFallbacksEnabled(False)
+        self.move(self.setting.value("pos", self.pos()))
+        self.resize(self.setting.value("size", self.size()))
+        font = self.font()
+        font.setPointSize(self.setting.value("font", 9, int))
+        self.setFont(font)
+
+        self.closeButton = QtWidgets.QPushButton("Set & Close", self)
+        self.uval_text = QtWidgets.QLineEdit("0.0", self)
+        self.vval_text = QtWidgets.QLineEdit("0.0", self)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.closeButton)
+        self.layout.addWidget(self.uval_text)
+        self.layout.addWidget(self.vval_text)
+        self.setLayout(self.layout)
+
+    def close_self(self):
+        if self.setting != None:
+            # Write window size and position to config file
+            self.setting.setValue("size", self.size())
+            self.setting.setValue("pos", self.pos())
+            self.setting.setValue("font", self.font().pointSize())
+        self.close()
+
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", dest="dir", default="./")
+    parser.add_argument("--pxyz", dest="pxyz",
+                        default=[0.0, 0.0, 0.0], type=float, nargs=3)
+    opt = parser.parse_args()
+    print(opt)
+
+    print("Qt version:", QtCore.QT_VERSION_STR)
+    print("PyQt version:", QtCore.PYQT_VERSION_STR)
+    print("SIP version:", sip.SIP_VERSION_STR)
--- torus_dev.py
+++ torus_dev.py
@@ -5,11 +5,15 @@ import os
 import time
 import argparse
 
+basename = os.path.dirname(__file__) + "/"
+
 sys.path.append(os.path.join("./"))
 from src.base_occ import dispocc
 
 import logging
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
@@ -27,10 +31,48 @@ from OCCUtils.Construct import make_n_sided, make_n_sections
 from OCCUtils.Construct import make_edge
 
 
-class Torus (dispocc):
+class NewWidget(QtWidgets.QWidget):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.left = 200
+        self.top = 200
+        self.width = 125
+        self.height = 125
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.setting = QtCore.QSettings(basename + "temp_setting/Widget.ini",
+                                        QtCore.QSettings.IniFormat)
+        self.setting.setFallbacksEnabled(False)
+        self.move(self.setting.value("pos", self.pos()))
+        self.resize(self.setting.value("size", self.size()))
+        font = self.font()
+        font.setPointSize(self.setting.value("font", 9, int))
+        self.setFont(font)
+
+        self.closeButton = QtWidgets.QPushButton("Set & Close", self)
+        self.uval_text = QtWidgets.QLineEdit("0.0", self)
+        self.vval_text = QtWidgets.QLineEdit("0.0", self)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.closeButton)
+        self.layout.addWidget(self.uval_text)
+        self.layout.addWidget(self.vval_text)
+        self.setLayout(self.layout)
+
+    def close_self(self):
+        if self.setting != None:
+            # Write window size and position to config file
+            self.setting.setValue("size", self.size())
+            self.setting.setValue("pos", self.pos())
+            self.setting.setValue("font", self.font().pointSize())
+        self.close()
+
+
+class Torus (dispocc, Geom_ToroidalSurface):
+
+    def __init__(self, temp=True, disp=True, touch=False):
+        super().__init__(temp, disp, touch)
         self.axs = gp_Ax3()
         self.radi = [500, 200]
         self.rxyz = [1.0, 1.1, 1.1]
@@ -44,7 +86,7 @@ class Torus (dispocc):
         self.face = BRepBuilderAPI_MakeFace(self.t, 1e-6).Face()
         self.face = BRepBuilderAPI_GTransform(self.face, gtrf).Shape()
         self.surf = BRep_Tool.Surface(self.face)
-        self.prop = GeomLProp_SLProps(self.surf, 0.0, 0.0, 1, 1.0)
+        self.prop = GeomLProp_SLProps(self.surf, 0.0, 0.0, 2, 0.1E-3)
         self.export_stp(self.face)
         print(self.t.UPeriod())
 
@@ -62,13 +104,28 @@ class Torus (dispocc):
         print(vx)
         print(vy)
         print(vz)
-        print(self.prop.GaussianCurvature())
-        print(self.prop.MaxCurvature())
-        print(self.prop.MinCurvature())
-        print(self.prop.MeanCurvature())
+        if self.prop.IsCurvatureDefined():
+            print("Gaussian :", 1 / self.prop.GaussianCurvature())
+            print("Max      :", 1 / self.prop.MaxCurvature())
+            print("Min      :", 1 / self.prop.MinCurvature())
+            print("Mean     :", 1 / self.prop.MeanCurvature())
+        else:
+            print("NO Curvature defined")
         self.display.DisplayShape(p)
         self.display.DisplayVector(vz.Scaled(20), p)
         return p, vu, vv, vz
+
+    def set_uv_widget(self):
+        self.uvWidget = NewWidget(None)
+
+        self.uvWidget.closeButton.clicked.connect(self.get_uv_val)
+        self.uvWidget.closeButton.clicked.connect(self.uvWidget.close_self)
+        self.uvWidget.show()
+
+    def get_uv_val(self):
+        u = self.text2float(self.uvWidget.uval_text.text(), 0.0)
+        v = self.text2float(self.uvWidget.vval_text.text(), 0.0)
+        self.get_prof([u, v])
 
     def ShowTorus(self):
         self.display.DisplayShape(self.face, transparency=0.7)
@@ -84,6 +141,10 @@ if __name__ == '__main__':
     print(opt, argvs)
 
     obj = Torus()
+    obj.add_menu("SPlop")
+    obj.add_function("SPlop", obj.set_uv_widget)
     obj.get_prof([0, 0])
+    obj.get_prof([0, 0.3])
+    obj.get_prof([0.1, 0])
     obj.get_prof([0.1, 0.3])
     obj.ShowTorus()
