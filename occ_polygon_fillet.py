951506d973a31d1704d47868241e0fc93b36d898
tnakaicode <tnakaicode@gmail.com>
2023-11-21 14:53:25 +0900

    make polygon

---
+++ occ_polygon_fillet.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+import argparse
+from linecache import getline, clearcache, updatecache
+
+basename = os.path.dirname(__file__)
+
+sys.path.append(os.path.join("./"))
+from src.base_occ import dispocc
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
+from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.Core.TopoDS import TopoDS_Shape, TopoDS_Compound
+from OCC.Core.TopLoc import TopLoc_Location
+from OCC.Extend.TopologyUtils import TopologyExplorer
+from OCCUtils.Common import vertex2pnt
+from OCCUtils.Construct import make_box, make_line, make_wire, make_edge
+from OCCUtils.Construct import make_plane, make_polygon
+from OCCUtils.Construct import point_to_vector, vector_to_point
+from OCCUtils.Construct import dir_to_vec, vec_to_dir
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", dest="dir", default="./")
+    parser.add_argument("--pxyz", dest="pxyz",
+                        default=[0.0, 0.0, 0.0], type=float, nargs=3)
+    opt = parser.parse_args()
+    print(opt)
+
+    obj = dispocc(touch=True)
+    axs = gp_Ax3()
+
+    pts = [
+        gp_Pnt(-50, -50, 0),
+        gp_Pnt(50, -50, 0),
+        gp_Pnt(50, 50, 0),
+        gp_Pnt(0, 60, 0),
+        gp_Pnt(-50, 50, 0),
+    ]
+    edg = []
+    for i, p in enumerate(pts):
+        i0, i1 = i, (i + 1) % (len(pts))
+        edg.append(make_edge(pts[i0], pts[i1]))
+
+    poly = make_wire(edg)
+    obj.display.DisplayShape(poly)
+    obj.ShowOCC()
