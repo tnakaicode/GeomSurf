eb0988f7f83819fafaedbbc5f071a1b18fdd8b6e
tnakaicode <tnakaicode@gmail.com>
2023-11-30 19:05:42 +0900

    pnt sphere

---
+++ occ_pnt_sphere.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+import argparse
+from linecache import getline, clearcache, updatecache
+
+basename = os.path.dirname(__file__)
+
+sys.path.append(os.path.join("./"))
+from src.base_occ import dispocc
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
+from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.Core.TopoDS import TopoDS_Shape, TopoDS_Compound
+from OCC.Core.TopLoc import TopLoc_Location
+from OCC.Extend.TopologyUtils import TopologyExplorer
+from OCCUtils.Common import vertex2pnt
+from OCCUtils.Construct import make_box, make_line, make_wire, make_edge
+from OCCUtils.Construct import make_plane, make_polygon
+from OCCUtils.Construct import point_to_vector, vector_to_point
+from OCCUtils.Construct import dir_to_vec, vec_to_dir
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", dest="dir", default="./")
+    parser.add_argument("--pxyz", dest="pxyz",
+                        default=[0.0, 0.0, 0.0], type=float, nargs=3)
+    opt = parser.parse_args()
+    print(opt)
+
+    obj = dispocc(touch=True)
+    axs = gp_Ax3()
+
+    n = 100
+    xyz = np.random.normal(0.0, 1.0, [n, 3])
+    for p in xyz:
+        obj.display.DisplayShape(gp_Pnt(*p))
+
+    obj.show_axs_pln(scale=1)
+    obj.ShowOCC()
