0f37b04bccd9cbfa27e0d9a739701737cfa050ec
tnakaicode <tnakaicode@gmail.com>
2023-12-16 01:22:09 +0900

    fillet

--- occ_curve_fillet.py
+++ occ_curve_fillet.py
@@ -61,6 +61,39 @@ def edge_endpoint(edg=TopoDS_Edge()):
     return tool.Value(BRepAdaptor_Curve(edg), u1)
 
 
+def edge_rndpont(edg=TopoDS_Edge(), n=3):
+    from OCC.Core.BRepGProp import BRepGProp_EdgeTool
+    from OCC.Core.BRepAdaptor import BRepAdaptor_Curve
+    from OCC.Core.BRepGProp import brepgprop
+    from OCC.Core.GProp import GProp_GProps
+    from OCC.Core.GeomAPI import GeomAPI_PointsToBSpline
+    from OCC.Core.TColgp import TColgp_Array1OfPnt
+    tool = BRepGProp_EdgeTool()
+    c = BRepAdaptor_Curve(edg)
+    u0 = tool.FirstParameter(c)
+    u1 = tool.LastParameter(c)
+    if n == 2:
+        return edg
+    elif n >= 2:
+        prop = GProp_GProps()
+        brepgprop.LinearProperties(edg, prop)
+        lth = prop.Mass()
+        pts = TColgp_Array1OfPnt(1, n)
+        for i, t in enumerate(np.linspace(u0, u1, n)):
+            p = tool.Value(c, t)
+            x = p.X() + np.random.uniform(0, lth / 10)
+            y = p.Y() + np.random.uniform(0, lth / 10)
+            pts.SetValue(i + 1, gp_Pnt(x, y, p.Z()))
+        pts.SetValue(1, tool.Value(c, u0))
+        pts.SetValue(n, tool.Value(c, u1))
+        curv = GeomAPI_PointsToBSpline(pts).Curve()
+        return make_edge(curv, 0, 1)
+    elif n == 0:
+        return tool.Value(c, u1)
+    elif n == 1:
+        return tool.Value(c, u1)
+
+
 def make_fillet(e1=TopoDS_Edge(), e2=TopoDS_Edge(), radii=10, pln=gp_Pln()):
     f = ChFi2d_AnaFilletAlgo()
     f.Init(e1, e2, pln)
@@ -96,90 +129,11 @@ if __name__ == '__main__':
     ]
 
     pts = [gp_Pnt(*dat) for (dat, r) in data]
-    rad = [r for (dat, r) in data]
+    pln = gp_Pln(axs)
     edg = [make_edge(pts[i], pts[(i + 1) % len(pts)]) for i in range(len(pts))]
-    fil = []
-    pln = gp_Pln()
-    radii = 5.0
-    poly = make_wire(edg)
-    obj.display.DisplayShape(edg, color="BLUE1")
-
-    edg = [make_edge(pts[0], pts[1])]
-    for i, p in enumerate(pts + [pts[0]]):
-        i1, i2 = (i + 1) % (len(pts)), (i + 2) % (len(pts))
-        radii = rad[i1]
-        e1, e2 = edg[-1], make_edge(pts[i1], pts[i2])
-        edges = make_fillet(e1, e2, radii, pln)
-        edg[-1] = edges[0]
-        edg += edges[1:]
-    edg.pop(0)
-    edg.pop(-1)
-    edg.pop(-1)
-
-    # f = ChFi2d_AnaFilletAlgo()
-    # edg = []
-    # e0 = make_edge(pts[0], pts[1])
-    # e1 = make_edge(pts[1], pts[2])
-    # f.Init(e0, e1, pln)
-    # f.Perform(radii)
-    # f01 = f.Result(e0, e1)
-    # edg += [e0, f01, e1]
-    # print("0->1->2", pts[0], pts[1], pts[2], edge_length(f01))
-    # print("e0->f01", edge_endpoint(e0), edge_1stpoint(f01))
-    # print("f01->e1", edge_1stpoint(e1), edge_endpoint(f01))
-
-    # for i, p in enumerate(pts):
-    #    i1, i2, i3 = i, (i + 1) % (len(pts)), (i + 2) % (len(pts))
-    #    p1, p2, p3 = pts[i1], pts[i2], pts[i3]
-#
-    #    # Making the edges
-    #    ed1 = make_edge(p3, p2)
-    #    ed2 = make_edge(p2, p1)
-#
-    #    # Making the 2dFillet
-    #    f = ChFi2d_AnaFilletAlgo(ed1, ed2, pln)
-    #    f.Perform(radii)
-    #    fillet2d = f.Result(ed1, ed2)
-#
-    #    #edg += [ed1, fillet2d]
-    #    obj.display.DisplayShape(make_wire([ed1, fillet2d, ed2]), color=obj.colors[0])
-
-    # for i, e in enumerate(edg[:2]):
-    #    print(i, e)
-    #    print(edge_length(edg[i]), edge_length(edg[i+1]))
-    #    f.Init(edg[i].Reversed(), edg[i+1].Reversed(), pln)
-    #    #try:
-    #    #    f.Init(edg[i], edg[i+1], pln)
-    #    #except:
-    #    #    f.Init(edg[i].Reversed(), edg[i+1], pln)
-    #    print(f.Perform(radii))
-    #    fi = f.Result(edg[i], edg[i+1])
-    #    print(edge_length(fi))
-    #    print(edge_length(edg[i]), edge_length(edg[i+1]))
-
-    # e2 = make_edge(pts[2], pts[3])
-    # print("e1", edge_1stpoint(e1), edge_endpoint(e1))
-    # print("e2", edge_1stpoint(e2), edge_endpoint(e2))
-    # f = ChFi2d_AnaFilletAlgo()
-    # f.Init(e1, e2, pln)
-    # f.Perform(radii)
-    # f12 = f.Result(e1, e2)
-    # edg += [f12, e2]
-    # print("1->2->3", pts[1], pts[2], pts[3], edge_length(f12))
-    # print("e1->f12", edge_endpoint(e1), edge_1stpoint(f12))
-    # print("f12->e3", edge_1stpoint(e2), edge_endpoint(f12))
-
-    # e2, e3 = edg[-1], make_edge(pts[4], pts[3])
-    # f.Init(e2, e3, pln)
-    # f.Perform(radii)
-    # f23 = f.Result(e2, e3)
-    # edg += [f23, e3]
-    #
-    # edg.insert(1, fil[0])
-    # edg.insert(3, fil[1])
+    edg[0] = edge_rndpont(edg[0], 5)
 
     poly = make_wire(edg)
-    obj.display.DisplayShape(poly)
-    obj.display.DisplayShape(make_face(poly))
-    [obj.display.DisplayShape(edge_midpoint(e)) for e in edg + fil]
+    obj.display.DisplayShape(poly, color="BLUE1")
+
     obj.ShowOCC()
