deec7a031445362a974c58ceaca055531ba988fb
tnakaicode <tnakaicode@gmail.com>
2024-03-19 20:29:44 +0900

    cylinder hole

--- occ_cylinder_hole.py
+++ occ_cylinder_hole.py
@@ -39,15 +39,15 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt, argvs)
 
-    obj = dispocc(touch=True)
+    obj = dispocc(touch=False)
     axs = gp_Ax3()
     surf = Geom_CylindricalSurface(gp_Ax3(), 100)
-    face = make_face(surf.Cylinder(), 0, np.pi, -50, 50)
+    face = make_face(surf.Cylinder(), 0, np.pi, -10, 50)
 
     dat = [
-        [np.pi / 8, 0],
-        [np.pi / 4, 0],
-        [np.pi / 4, 10],
+        [np.pi / 8, 5],
+        [np.pi / 4, 8],
+        [np.pi / 6, 10],
         [np.pi / 8, 10],
     ]
     edg = []
@@ -58,28 +58,33 @@ if __name__ == '__main__':
         ci = GCE2d_MakeSegment(li, p0, p1).Value()
         ei = BRepBuilderAPI_MakeEdge(ci, surf, 0, p1.Distance(p0)).Edge()
         print(ei, ci, p1.Distance(p0))
-        obj.display.DisplayShape(ei)
+        # obj.display.DisplayShape(ei)
         edg.append(ei)
     rim = make_wire(edg)
 
-    # api_face = BRepAlgo_FaceRestrictor()
-    # api_face.Init(face, False, True)
-    # api_face.Add(rim)
-    # api_face.Perform()
-    #
-    # while api_face.More():
-    #    obj.display.DisplayShape(api_face.Current())
-    #    api_face.Next()
-
-    # rim = make_polygon([gp_Pnt(0,0,0), gp_Pnt(20, 0,0), gp_Pnt(30, 0,20), gp_Pnt(0, 0,20)], closed=True)
-    # rim_proj = obj.proj_rim_pln(rim, face, gp_Ax3(gp_Pnt(0,0,0), gp_Dir(0,1,0)), 1)
-    api_face = BRepBuilderAPI_MakeFace()
-    api_face.Init(face)
+    api_face = BRepAlgo_FaceRestrictor()
+    api_face.Init(face, False, True)
     api_face.Add(rim)
-    api_face.Build()
+    api_face.Perform()
     print(api_face.IsDone())
-    face = api_face.Shape()
+    while api_face.More():
+        print(api_face.Current())
+        # obj.display.DisplayShape(api_face.Current(), transparency=0.9)
+        api_face.Next()
+
+    rim = make_polygon([gp_Pnt(0, 0, 0), gp_Pnt(20, 0, 0),
+                       gp_Pnt(30, 0, 20), gp_Pnt(0, 0, 20)], closed=True)
+    rim_proj = obj.proj_rim_pln(rim, face, gp_Ax3(gp_Pnt(0, 0, 0),
+                                                  gp_Dir(0, 1, 0)), 1)
+    api_face = BRepBuilderAPI_MakeFace(surf, rim_proj)
+    # api_face.Init(face)
+    # api_face.Add(rim)
+    api_face.Build()
+    # print(api_face.IsDone())
+    # face = api_face.Shape()
     # face = make_face(face, rim)
-    obj.export_stp(face)
+    # obj.export_stp(face)
+    obj.display.DisplayShape(rim)
     obj.display.DisplayShape(face)
+    obj.display.DisplayShape(api_face.Shape(), color="RED")
     obj.ShowOCC()
