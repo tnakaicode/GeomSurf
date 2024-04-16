1220d0ef9a44a393e0295b2f39b95402b1b92b3b
tnakaicode <tnakaicode@gmail.com>
2024-04-16 18:16:54 +0900

    torus

--- occ_torus.py
+++ occ_torus.py
@@ -16,6 +16,7 @@ logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.Core.gp import gp_Trsf
 from OCC.Core.gp import gp_Circ
 from OCC.Core.BRepAdaptor import BRepAdaptor_Surface
 from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_MakePipe
@@ -39,24 +40,49 @@ if __name__ == '__main__':
 
     obj = dispocc(touch=True)
     axs = gp_Ax3()
-
-    major = 2000
-    minor = 1000
-    radii = major + minor
-
-    axs_major = gp_Ax2(gp_Pnt(0, 0, 0.0), gp_Dir(0, 0, 1), gp_Dir(1, 0, 0))
-    axs_minor = gp_Ax2(gp_Pnt(0, major, 0.0), gp_Dir(1, 0, 0), gp_Dir(0, 1, 0))
     tok = gp_Ax3(gp_Pnt(0, 0, 0.0), gp_Dir(0, 1, 0), gp_Dir(-1, 0, 0))
 
-    cir_major = make_edge(gp_Circ(axs_major, major), -np.pi / 2, np.pi / 2)
-    cir_minor = make_edge(gp_Circ(axs_minor, minor), -np.pi / 2, np.pi / 2)
+    z = 0
+    major = 2000
+    minor = 5000
+    radii = 3000
+
+    pnt_major = gp_Pnt(0, 0, z)
+    axs_major = gp_Ax2(pnt_major, gp_Dir(0, 0, 1), gp_Dir(1, 0, 0))
+    rng_major = [0 + np.pi / 3, np.pi - np.pi / 3]
+
+    pnt_minor = gp_Pnt(0, radii - minor, z)
+    axs_minor = gp_Ax2(pnt_minor, gp_Dir(1, 0, 0), gp_Dir(0, 1, 0))
+    rng_minor = [-np.pi / 2 + np.pi / 3, np.pi / 2 - np.pi / 3]
+
+    cir_major = make_edge(gp_Circ(axs_major, major), *rng_major)
+    cir_minor = make_edge(gp_Circ(axs_minor, minor), *rng_minor)
     api = BRepOffsetAPI_MakePipe(make_wire(cir_major), cir_minor)
     api.Build()
     shll = api.Shape()
     face = [f for f in TopologyExplorer(shll).faces()][0]
     surf = BRepAdaptor_Surface(face)
 
-    obj.show_axs_pln()
+    trsf = gp_Trsf()
+    trsf.SetDisplacement(gp_Ax3(), tok)
+    nu, nv = 3, 4
+    us, ue = surf.FirstUParameter(), surf.LastUParameter()
+    vs, ve = surf.FirstVParameter(), surf.LastVParameter()
+    pu = np.linspace(us, ue, nu)
+    pv = np.linspace(vs, ve, nv)
+    mesh = np.meshgrid(pu, pv)
+    data = np.zeros_like(mesh[0])
+    for i, u in enumerate(pu):
+        for j, v in enumerate(pv):
+            p0 = surf.Value(u, v)
+            obj.display.DisplayShape(p0)
+            p0.Transform(trsf)
+            print(p0)
+            # mesh[0][j, i] = p0.X()
+            # mesh[1][j, i] = p0.Y()
+            data[j, i] = p0.Z()
+
+    obj.show_axs_pln(tok)
     obj.display.DisplayShape(shll)
     obj.display.DisplayShape(cir_minor)
     obj.display.DisplayShape(cir_major)
