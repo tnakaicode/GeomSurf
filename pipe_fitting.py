9e971c9a9bbfa8ce219dee8bb6f5a9398dadc22b
tnakaicode <tnakaicode@gmail.com>
2024-07-29 01:01:41 +0900

    pipe

--- pipe_fitting.py
+++ pipe_fitting.py
@@ -17,7 +17,7 @@ logging.getLogger('matplotlib').setLevel(logging.ERROR)
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
 from OCC.Core.gp import gp_Circ
-from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_MakePipe
+from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_MakePipe, BRepOffsetAPI_MakePipeShell
 from OCC.Core.TopoDS import TopoDS_Shape, TopoDS_Compound
 from OCC.Core.TopLoc import TopLoc_Location
 from OCC.Extend.TopologyUtils import TopologyExplorer
@@ -38,11 +38,16 @@ if __name__ == '__main__':
 
     obj = dispocc(touch=True)
     axs = gp_Ax3()
+    
+    circ = make_edge(gp_Circ(gp_Ax2(gp_Pnt(0, 10, 0), gp_Dir(1,0,0)), 10), np.pi/2, np.pi/2+np.pi/4)
+    edg1 = make_edge(gp_Pnt(), gp_Pnt(0, 0, 100))
 
-    wire = make_wire(make_edge(gp_Pnt(), gp_Pnt(0, 0, 100)))
+    wire = make_wire(make_edge(gp_Pnt(), gp_Pnt(0, 0, 100)), circ)
     face = make_face(make_wire(make_edge(gp_Circ(gp_Ax2(), 50))))
     cylinder = BRepOffsetAPI_MakePipe(wire, face).Shape()
 
     obj.display.DisplayShape(cylinder, transparency=0.9)
-    obj.show_axs_pln()
+    obj.display.DisplayShape(circ)
+    obj.display.DisplayShape(edg1)
+    #obj.show_axs_pln()
     obj.ShowOCC()
